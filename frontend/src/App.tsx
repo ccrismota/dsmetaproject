bb1944e37876164c0acd3855ddc5c2b9eec69902
ccrismota <ccrismota@gmail.com>
2022-09-06 00:24:20 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificationButton from "./componets/NotificationButton";
+
 function App() {
   return (
-    <h1>Olá mundo</h1>
-  )
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
